3094f257b3418b8bcedf346a51dfeff1dcc2fec1
Stanley Louis <stanlouis@msn.com>
2019-01-21 14:16:47 -0500

    Add navbar content and styles.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.scss';
+import Navbar from './components/Navbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
         <h1>Hello from App</h1>
       </div>
     );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,1 +1,32 @@
+import React from 'react';
+import './Navbar.scss';
+import logo from '../logo.png';
 
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-md navbar-dark">
+      <a href="#home" className="navbar-brand">
+        <img src={logo} alt="logo" />
+      </a>
+      <ul className="navbar-nav">
+        <li className="nav-item active">
+          <a className="nav-link" href="#home">
+            Home <span class="sr-only">(current)</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a className="nav-link" href="#about">
+            About
+          </a>
+        </li>
+        <li class="nav-item">
+          <a className="nav-link" href="#tours">
+            Tours
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
